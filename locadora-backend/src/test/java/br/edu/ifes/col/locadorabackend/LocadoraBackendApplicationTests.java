1bfc7d458b532010348f2cca5a9a07bd101f7d19
julionardi <julionardi@gmail.com>
2022-08-29 08:49:12 -0300

    Criação do projeto do backend (locadora-backend) no Spring Tool Suite 4.0

---
+++ locadora-backend/src/test/java/br/edu/ifes/col/locadorabackend/LocadoraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifes.col.locadorabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
